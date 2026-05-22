4b93a9ae8f45c6c8c37439b55eef2ad9234f710b
Gab <fga.080272@gmail.com>
2026-05-22 22:02:24 +0800

    addTask Input logic

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 // ADD TASK INPUT & BUTTON
-const addTaskInput = document.getElementById("add-taski-input");
+const addTaskInput = document.getElementById("add-task-input");
 const addTaskButton = document.getElementById("add-task-button");
 
 // TASK LIST
 const taskStatus = document.getElementById("task-status");
+const taskCheck = document.getElementById("task-check");
 const taskName = document.getElementById("task-name");
 const removeTask = document.getElementById("remove-task");
 
 // FETCH NEW TASK INPUT
-
-/* addTaskButton button logic:
-    addTaskButton.addEventListener("click", (event) => {
-        console.log('button clicked')
-        event.preventDefault()
-        code
-    });
-*/
+const addTask = addTaskButton.addEventListener("click", (event) => {
+  console.log("button clicked");
+  console.log(addTaskInput.value);
+});
