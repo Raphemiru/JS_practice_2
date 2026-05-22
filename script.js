5a49bfb36daa3b8c77ffa8d25fa1b071c889a7a6
Gab <fga.080272@gmail.com>
2026-05-22 16:52:24 +0800

    JS inititalize const

---
+++ script.js
@@ -0,0 +1,10 @@
+// ADD TASK INPUT & BUTTON
+const addTaskInpug = document.getElementById("add-task");
+const addTaskButton = document.getElementById("add-task-button");
+
+// TASK LIST
+const taskStatus = document.getElementById("task-status");
+const taskName = document.getElementById("task-name");
+const removeTask = document.getElementById("remove-task");
+
+// FETCH NEW TASK INPUT
