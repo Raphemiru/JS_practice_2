5db87a3ba00dc20277c5e5aff0f58f0d4b342494
Gab <fga.080272@gmail.com>
2026-05-23 12:29:16 +0800

    addTask visible HTML

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ const addTaskInput = document.getElementById("add-task-input");
 const addTaskButton = document.getElementById("add-task-button");
 
 // ADD NEW TASK
-addTaskButton.addEventListener("click", () => {
-  // code to create new list using const task
-  //
-  console.log(addTaskInput.value); // checks if the input text gets noticed
-});
+const taskList = document.getElementById("task-list");
 
-/* TASK CODE
-const task = () => {
-    <li> class="task" id="task">
-        <div class="task-check" id="task-check">
-            <img id="task-status" src="" alt="" />
-            <span id="task-name"></span>
-        </div>
-        <button id="remove-task" class="remove-task"></button>
-    </li>
-}
-*/
+const addTask = () => {
+  const li = document.createElement("li");
+  li.className = "task";
+  li.id = "task";
+
+  const div = document.createElement("div");
+  div.className = "task-check";
+  div.id = "task-check";
+
+  const img = document.createElement("img");
+  img.id = "task-status";
+  img.src = "";
+  img.alt = "";
+
+  const span = document.createElement("span");
+  span.id = "task-name";
+  span.textContent = addTaskInput.value;
+
+  const button = document.createElement("button");
+  button.textContent = "x";
+  button.id = "remove-task";
+  button.className = "remove-task";
+
+  div.appendChild(img);
+  div.appendChild(span);
+  li.appendChild(div);
+  li.appendChild(button);
+  taskList.appendChild(li);
+
+  addTaskInput.value = "";
+};
+
+addTaskButton.addEventListener("click", () => {
+  addTask();
+});
